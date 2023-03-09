c54bd7eaa10ea2264bf634c693f57679cfc90d5f
Rafsanjani N.I <45141323+maplezs@users.noreply.github.com>
2023-03-10 01:16:55 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -69,9 +69,8 @@ VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 
 def get_authenticated_service(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-    scope=YOUTUBE_UPLOAD_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+  flow = InstalledAppFlow.from_client_config(CLIENT_SECRETS_FILE, YOUTUBE_UPLOAD_SCOPE)
+  credentials = flow.run_console()
 
   storage = Storage("%s-oauth2.json" % sys.argv[0])
   credentials = storage.get()
